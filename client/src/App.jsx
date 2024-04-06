8e5f2f597b64eb9d7bf46175b3ef659d470949b2
Partha Mallick <partha0988@gmail.com>
2024-04-06 08:41:07 +0530

    server down

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MaintenancePage from './MaintenancePage'
+// import LinkForm from "./components/LinkForm";
+
+
+const App = () => {
+  return (
+    <div>
+      <MaintenancePage />
+      {/* <LinkForm /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/MaintenancePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MaintenancePage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">Server Down</h1>
+        <p className="text-lg mb-8">We are currently undergoing maintenance. Please check back later.</p>
+      </div>
+    </div>
+  );
+};
+
+export default MaintenancePage;
